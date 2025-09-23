1b9d22710e82a4688791e4b678c99033073b7e84
angurgapi <sophiekovalsky@gmail.com>
2025-09-23 19:32:07 +0200

    fix footer width

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <div className="min-h-[60px] w-full flex">
-      <div className="wrapper__content wrapper__content--sided flex items-center flex-col md:flex-row md:justify-between justify-start bg-white text-slate-600 p-[20px]">
+      <div className="flex w-full items-center flex-col md:flex-row md:justify-between justify-start bg-white text-slate-600 p-[20px]">
         <a href="https://github.com/angurgapi" target="_blank">
           ©MyBookClub {new Date().getFullYear()}
         </a>{' '}
